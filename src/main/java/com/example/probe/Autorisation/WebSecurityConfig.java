898faf74c0b745925d7dc9c81ed5e13efab3829c
Stangaan <schtengauer@gmail.com>
2023-10-23 17:54:55 +0200

    Security System

---
+++ src/main/java/com/example/probe/Autorisation/WebSecurityConfig.java
@@ -0,0 +1,47 @@
+package com.example.securingweb;
+
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/", "/home").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .logout((logout) -> logout.permitAll());
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("user")
+                        .password("password")
+                        .roles("ADMIN")
+                        .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+}
--- src/main/java/com/example/probe/Controller/UserController.java
+++ src/main/java/com/example/probe/Controller/UserController.java
@@ -3,7 +3,7 @@ package com.example.probe.Controller;
 import com.example.probe.Entity.User;
 import com.example.probe.Service.UserService;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,16 +14,17 @@ public class UserController {
     private final UserService userService;
 
 @GetMapping("/user")
-public List<User> getAllUser(@RequestBody User user){return "<h2> Hello user<h2>" userService.getAllUsers();}
-        @GetMapping("/get-user{id}")
+public String hello(){return "<h2> Hello user</h2>";}
+        @GetMapping(
+                "/get-user{id}")
     public List<User> getUserById(@PathVariable Long id){
         return userService.getAllUsers();
         }
-        @PostMapping("/create-user")
+        @PostMapping("create-user")
     public User createUser(@RequestBody User user){
          return userService.createUser(user);
         }
-        @PutMapping("/update{id}")
+        @PutMapping("update{id}")
     public User updateUser (@PathVariable Long id, @RequestBody User updatedUser){
       return userService.updateUser(id, updatedUser);
         }
